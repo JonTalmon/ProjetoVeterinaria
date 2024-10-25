1cd554dde8cb082d2a7094c8b77f15fa01985228
Jônatas Talmon <jonatas_talmon@hotmail.com>
2024-10-24 21:24:41 -0300

    adding GET

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 import express from 'express'
-import { sequelize } from "./src/db/connection.js"
+import sequelize from "./src/db/connection.js"
 import dotenv from "dotenv";
 
 dotenv.config();
 
+const port = process.env.PORT || 3000;
 const app = express();
-const port = 3000;
+
+import PersonRegister from './src/models/personRegister.js';
+
+app.use(express.json());
 
 
-
-
-
-app.listen(port, async () => {
+app.get("/person", async (req, res) => {
     try {
-        await sequelize.authenticate();
-        console.log("Connected to the database");
-    } catch (err) {
-        console.error("Error connecting to the database", err);
+        const result = await PersonRegister.findAll(); // Busca todos os registros
+        console.log('Query Result:', JSON.stringify(result, null, 2)); // Verifique o resultado da consulta
+        res.status(200).json(result); // Retorna os registros encontrados
+    } catch (error) {
+        console.error('Error retrieving people:', error);
+        res.status(500).json({ error: 'Error retrieving people', details: error.message }); // Retorna erro se ocorrer
     }
-    console.log(`Server is running on port: ${port}`);
+});
+
+
+
+sequelize.sync().then(() => {
+    console.log("Connected to the database and tables syncronized");
+    app.listen(port, () => {
+        console.log(`Server is running on port: ${port}`);
+    });
+}).catch((error) => {
+    console.error("Error connecting to the database:", error);
 });
--- src/db/connection.js
+++ src/db/connection.js
@@ -1,20 +1,42 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
-dotenv.config();
+//dotenv.config();
 
-export const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         dialect: "postgres",
+//         dialectOptions:  {
+//             ssl: {
+//                 require: true,
+//                 rejectUnauthorized: false
+//             } 
+//         }
+// });
+
+const sequelize = new Sequelize(
+    'mascot_clinical_pp4h', // DB_NAME
+    'mascot_clinical_pp4h_user', // DB_USER
+    'x097KU27wDbDdYQB76D8ciTakP0SfGsc', // DB_PASSWORD
     {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        host: 'dpg-csbg1123esus73fpa7q0-a.oregon-postgres.render.com', // DB_HOST
+        port: 5432, // DB_PORT
         dialect: "postgres",
-        dialectOptions:  {
+        dialectOptions: {
             ssl: {
                 require: true,
                 rejectUnauthorized: false
-            } 
+            }
         }
-});
+    }
+);
+
+
+
+
+export default sequelize
